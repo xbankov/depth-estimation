bec6c7c69541a80685ae537b698bbb09070d2a44
mikulas <mikulas.bankovic27@gmail.com>
2023-08-22 12:09:33 +0200

    Initialization

---
+++ utils.py
@@ -0,0 +1,103 @@
+import os
+import shutil
+import tarfile
+import zipfile
+
+import h5py
+import numpy as np
+from PIL import Image
+from torchvision.datasets.utils import download_url
+
+
+def _create_depth_files(mat_file: str, root: str, train_ids: list):
+    """
+    Extract the depth arrays from the mat file into images
+    :param mat_file: path to the official labelled dataset .mat file
+    :param root: The root directory of the dataset
+    :param train_ids: the IDs of the training images as string (for splitting)
+    """
+    os.mkdir(os.path.join(root, "train_depth"))
+    os.mkdir(os.path.join(root, "test_depth"))
+    train_ids = set(train_ids)
+
+    depths = h5py.File(mat_file, "r")["depths"]
+    for i in range(len(depths)):
+        img = (depths[i] * 1e4).astype(np.uint16).T
+        id_ = str(i + 1).zfill(4)
+        folder = "train" if id_ in train_ids else "test"
+        save_path = os.path.join(root, f"{folder}_depth", id_ + ".png")
+        Image.fromarray(img).save(save_path)
+
+
+def _download_rgb(root: str):
+    train_url = "http://www.doc.ic.ac.uk/~ahanda/nyu_train_rgb.tgz"
+    test_url = "http://www.doc.ic.ac.uk/~ahanda/nyu_test_rgb.tgz"
+
+    def _proc(url: str, dst: str):
+        if not os.path.exists(dst):
+            tar = os.path.join(root, url.split("/")[-1])
+            if not os.path.exists(tar):
+                download_url(url, root)
+            if os.path.exists(tar):
+                _unpack(tar)
+                _replace_folder(tar.rstrip(".tgz"), dst)
+                _rename_files(dst, lambda x: x.split("_")[2])
+
+    _proc(train_url, os.path.join(root, "train_rgb"))
+    _proc(test_url, os.path.join(root, "test_rgb"))
+
+
+def _unpack(file: str):
+    """
+    Unpacks tar and zip, does nothing for any other type
+    :param file: path of file
+    """
+    path = file.rsplit(".", 1)[0]
+
+    if file.endswith(".tgz"):
+        tar = tarfile.open(file, "r:gz")
+        tar.extractall(path)
+        tar.close()
+    elif file.endswith(".zip"):
+        zipf = zipfile.ZipFile(file, "r")
+        zipf.extractall(path)
+        zipf.close()
+
+
+def download_depth(root: str):
+    url = (
+        "http://horatio.cs.nyu.edu/mit/silberman/nyu_depth_v2/nyu_depth_v2_labeled.mat"
+    )
+    train_dst = os.path.join(root, "train_depth")
+    test_dst = os.path.join(root, "test_depth")
+
+    if not os.path.exists(train_dst) or not os.path.exists(test_dst):
+        tar = os.path.join(root, url.split("/")[-1])
+        if not os.path.exists(tar):
+            download_url(url, root)
+        if os.path.exists(tar):
+            train_ids = [
+                f.split(".")[0] for f in os.listdir(os.path.join(root, "train_rgb"))
+            ]
+            _create_depth_files(tar, root, train_ids)
+
+
+def _rename_files(folder: str, rename_func: callable):
+    """
+    Renames all files inside a folder based on the passed rename function
+    :param folder: path to folder that contains files
+    :param rename_func: function renaming filename (not including path) str -> str
+    """
+    imgs_old = os.listdir(folder)
+    imgs_new = [rename_func(file) for file in imgs_old]
+    for img_old, img_new in zip(imgs_old, imgs_new):
+        shutil.move(os.path.join(folder, img_old), os.path.join(folder, img_new))
+
+
+def _replace_folder(src: str, dst: str):
+    """
+    Rename src into dst, replacing/overwriting dst if it exists.
+    """
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    shutil.move(src, dst)
